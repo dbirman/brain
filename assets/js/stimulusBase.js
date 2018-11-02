07257a9c4f02a90c291073432416421ab7ff4991
Dan Birman <danbirman@gmail.com>
2018-11-02 07:59:33 -0700

    Remove null check
    - Setting the null check was a mistake -- it causes you to be unable to move faster than the refresh rate distance without losing the target. Have to find a different way to fix this issue.

--- assets/js/stimulusBase.js
+++ assets/js/stimulusBase.js
@@ -189,7 +189,7 @@ class Stimulus extends DContainer {
 	}
 
 	move(event) {
-		if (event.target==null) {return;}
+		// if (event.target==null) {return;}
 	  if (this.isdown && !this.localPreventMotion) {
 			this.moved = true;
 	    let mpos = event.data.getLocalPosition(this.parent);
