a67b2a571c42cc0740b76193ba4e56b1357b3dc2
Dan Birman <danbirman@gmail.com>
2018-11-02 07:25:59 -0700

    Fixed a major bug in stimulusBase.move()
     - For some reason extra events were firing with target=='null' even though the target was at the specified location (as if the mouse was moving to a location y+~100 pixels). These were causing the view to jitter, apparently you can filter these out simply by checking for event.target==null which occurs when the mouse is not over the stimulus. Not a full fix of the issue but good enough.

--- assets/js/stimulusBase.js
+++ assets/js/stimulusBase.js
@@ -189,6 +189,7 @@ class Stimulus extends DContainer {
 	}
 
 	move(event) {
+		if (event.target==null) {return;}
 	  if (this.isdown && !this.localPreventMotion) {
 			this.moved = true;
 	    let mpos = event.data.getLocalPosition(this.parent);
