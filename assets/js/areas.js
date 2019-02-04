4c5ac14c9ee4ae0db20b15aa1a89ad50106a3334
Akshay Jagadeesh <akjags@gmail.com>
2019-02-03 19:18:35 -0800

    Adding multiplicative gain V1,V2,MT response when RF overlaps with FEF neuron's RF

--- assets/js/areas.js
+++ assets/js/areas.js
@@ -1,4 +1,5 @@
 let maxFire = 50;
+let attGain = 1.75;
 
 // The data code uses the function mappings to communicate which response function
 // to use for contrast/coherence sensitivity. The parameters are stored by area.
@@ -103,6 +104,9 @@ function responseMT(elec,stim) {
 		response *= sens * con * 0.1;
 	}
 
+	if (elec.spatialAttention!=undefined && elec.spatialAttention==true) {
+    response *= attGain;
+  }
 	response = response<0.2 ? -1 : response;
 
 	return response;
@@ -128,7 +132,9 @@ function responseV1(elec,stim) {
 		response *= con * 0.15;
 	}
 
-	// if elec.spatialAttention==true multiplicative gain
+	if (elec.spatialAttention!=undefined && elec.spatialAttention==true) {
+    response *= attGain;
+  }
 
 	response = response<0.2 ? -1 : response;
 
@@ -216,6 +222,9 @@ function responseV2(elec,stim) {
 		response *= con * 0.15;
 	}
 
+	if (elec.spatialAttention!=undefined && elec.spatialAttention==true) {
+    response *= attGain;
+  }
 	response = response<0.2 ? -1 : response;
 
 	return response;
